7beecbe5463beeb1a367ddd01aa65731e596cc81
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2017-10-16 20:10:16 +0200

    add Schedule type

--- src/Schedule.hs
+++ src/Schedule.hs
@@ -1,6 +1,41 @@
 
 module Schedule (
-
+    Schedule(..),
+    isActive,
+    nextUpdate
 ) where
 
+import TimeUtil (endOfTime)
+import qualified RecurringPattern
+
 data Schedule = Schedule UTCTime UTCTime [RecurringPattern]
+
+endTime :: Schdedule -> UTCTime
+endTime (Schedule _ scheduleEndTime _) = scheduleEndTime
+
+nextUpdate :: Schedule -> UTCTime -> UTCTime
+nextUpdate schedule currentTime
+    | isActive schedule                 = nextUpdateActive schedule currentTime
+    | otherwise                         = nextUpdateInactive schedule currentTime
+
+nextUpdateActive :: schedule -> utctime -> utctime
+nextUpdateActive schedule currenttime
+    | nexttime > endtime schedule   = endtime schedule
+    | otherwise                     = nexttime
+    where   nexttime = nextupdateinternal schedule
+
+nextUpdateInactive :: schedule -> utctime -> utctime
+nextUpdateInactive schedule currenttime
+    | nexttime > endtime schedule   = endOfTime
+    | otherwise                     = nexttime
+    where   nexttime = nextupdateinternal schedule
+
+nextUpdateInternal :: Schedule -> UTCTime -> UTCTime
+nextUpdateInternal (Schedule scheduleStartTime scheduleEndTime recurringPatterns) =
+    RecurringPattern.nextUpdate scheduleStartTime scheduleEndTime recurringPatterns
+
+isActive :: Schedule -> UTCTime -> Bool
+isActive (Schedule scheduleStartTime scheduleEndTime recurringPatterns) currentTime
+    | currentTime >= scheduleEndTime    = False
+    | otherwise                         = calculatedState
+    where calculatedState = RecurringPattern.IsActive scheduleStartTime currentTime recurringPatterns
